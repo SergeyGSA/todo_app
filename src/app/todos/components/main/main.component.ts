b8bb17b9ec988a465d882e86fb933cc902fdf3a7
SergeyGSA <goreslavskiy.sergey@gmail.com>
2021-12-03 21:14:11 +0200

    todo component

--- src/app/todos/components/main/main.component.ts
+++ src/app/todos/components/main/main.component.ts
@@ -1,53 +1,54 @@
-import { Component, OnInit } from '@angular/core';
-import { combineLatest, map, Observable } from 'rxjs';
+import { Component } from '@angular/core';
+import { Observable, combineLatest, map } from 'rxjs';
 
+import { TodoInterface } from '../../types/todos.interface';
 import { TodosService } from '../../services/todos.service';
 import { FilterEnum } from '../../types/filter.enum';
-import { TodoInterface } from '../../types/todos.interface';
 
 @Component({
   selector: 'app-todos-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
 })
-export class MainComponent implements OnInit {
+
+export class MainComponent {
   visibleTodos$: Observable<TodoInterface[]>
   noTodoClass$: Observable<boolean>
   isAllTodosSelected$: Observable<boolean>
+  editingId: string | null = null
 
   constructor(
     private todosService: TodosService
-  ) 
-  { 
+  )
+  {
     this.isAllTodosSelected$ = this.todosService.todos$.pipe(
-      map((todos => todos.every(todo => todo.isCompleted))
-    ))
+      map((todos) => todos.every((todo) => todo.isCompleted))
+    )
 
     this.noTodoClass$ = this.todosService.todos$.pipe(
-      map(todos => todos.length === 0)
+      map((todos) => todos.length === 0)
     )
 
     this.visibleTodos$ = combineLatest(
       this.todosService.todos$,
       this.todosService.filter$
-    )
-    .pipe(
+    ).pipe(
       map(([todos, filter]: [TodoInterface[], FilterEnum]) => {
         if (filter === FilterEnum.active) {
-          return todos.filter(todo => !todo.isCompleted)
+          return todos.filter((todo) => !todo.isCompleted)
         } else if (filter === FilterEnum.completed) {
-          return todos.filter(todo => todo.isCompleted)
+          return todos.filter((todo) => todo.isCompleted)
         }
         return todos
       })
     )
   }
 
-  ngOnInit(): void {
-  }
-
-  toggleAllTodo(event: Event): void {
+  toggleAllTodos(event: Event): void {
     const target = event.target as HTMLInputElement
     this.todosService.toggleAll(target.checked)
+  }
+
+  setEditingId(editingId: string | null): void {
+    this.editingId = editingId
   }
 }
--- src/app/todos/components/todo/todo.component.ts
+++ src/app/todos/components/todo/todo.component.ts
@@ -1,17 +1,66 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnInit,
+  ElementRef,
+  ViewChild,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { TodoInterface } from '../../types/todos.interface';
+import { TodosService } from '../../services/todos.service';
 
 @Component({
   selector: 'app-todos-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
 })
-export class TodoComponent implements OnInit {
-  @Input('todo') todoProps!: TodoInterface
+export class TodoComponent implements OnInit, OnChanges {
+  @Input('todo') 
+    todoProps!: TodoInterface
+  @Input('isEditing') 
+    isEditingProps!: boolean
+  @Output('setEditingId') 
+    setEditingIdEvent: EventEmitter<string | null> = new EventEmitter()
 
-  constructor() { }
+  editingText: string = ''
+  @ViewChild('textInput') 
+    textInput!: ElementRef
+
+  constructor(private todosService: TodosService) {}
 
   ngOnInit(): void {
+    this.editingText = this.todoProps.text
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['isEditingProps'].currentValue) {
+      setTimeout(() => {
+        this.textInput.nativeElement.focus()
+      }, 0)
+    }
+  }
+
+  setTodoInEditMode(): void {
+    this.setEditingIdEvent.emit(this.todoProps.id)
+  }
+
+  removeTodo(): void {
+    this.todosService.removeTodo(this.todoProps.id)
+  }
+
+  toggleTodo(): void {
+    this.todosService.toggleTodo(this.todoProps.id)
+  }
+
+  changeText(event: Event): void {
+    const value = (event.target as HTMLInputElement).value
+    this.editingText = value
+  }
+
+  changeTodo(): void {
+    this.todosService.changeTodo(this.todoProps.id, this.editingText)
+    this.setEditingIdEvent.emit(null)
+  }
 }
